5031df19916ba698bcd6b42899038d6327f971ad
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-08-03 14:19:05 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -3,7 +3,7 @@ ctrl=pow(2,alt)
 zoo=[]
 for i in range(ctrl):
     mlt=bin(i).replace("0b","")
-    zoo.append(mlt.gfill(alt))
-    zoo.sort(key=lambada y:y count('l')))
+    zoo.append(mlt.zfill(alt))
+    zoo.sort(key=(lambada y:y.count('l')))
 for i in zoo:
     print(i)
