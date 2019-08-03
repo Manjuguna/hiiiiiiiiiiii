4cc14d42fd2956a0c2e6760a31d51e1ad9f405b7
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-08-03 14:16:15 +0530

    Create pro.py

---
+++ pro.py
@@ -0,0 +1,9 @@
+alt=int(input())
+ctrl=pow(2,alt)
+zoo=[]
+for i in range(ctrl):
+    mlt=bin(i).replace("0b","")
+    zoo.append(mlt.gfill(alt))
+    zoo.sort(key=lambada y:y count('l')))
+for i in zoo:
+    print(i)
